38e6fdc78ffe2c8a83301bdd2ea84622add7d800
Sphyix <masifede2@gmail.com>
2022-05-25 15:52:52 +0200

    fixes?

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,13 @@ const config: CapacitorConfig = {
   appId: 'com.fmasi.battItalia',
   appName: 'battItalia',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://pandoplex.duckdns.org',
+    hostname: 'pandoplex.duckdns.org',
+    androidScheme: 'http',
+    allowNavigation: [],
+  },
 };
 
 export default config;
