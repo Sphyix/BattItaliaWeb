30de6830e6a558700751cbc65756aa4dc9926ab3
Sphyix <masifede2@gmail.com>
2022-05-27 12:56:14 +0200

    add classes

---
+++ src/app/common/models/client-select-results.ts
@@ -0,0 +1,17 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export class ClientSelectResults {
+    clients_id: number;
+    nome: string;
+    cognome: string;
+    telefono: string;
+    mail: string;
+    comune: string;
+    provincia: string;
+    regione: string;
+    via: string;
+    civico: string;
+}
---
+++ src/app/common/models/work-order-log-select.ts
@@ -0,0 +1,11 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+import { IWorkOrderLogSelect } from "./i-work-order-log-select";
+
+export class WorkOrderLogSelect implements IWorkOrderLogSelect {
+    username: string;
+    executionMessages: string;
+}
