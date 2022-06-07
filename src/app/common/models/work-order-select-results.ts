37331a124f8eac9829511ca0e725d13055359145
Sphyix <masifede2@gmail.com>
2022-06-07 17:12:32 +0200

    update models

---
+++ src/app/common/models/work-order-select-results.ts
@@ -0,0 +1,22 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+
+export class WorkOrderSelectResults {
+    workOrders_id: number;
+    clientId: number;
+    tipoOggetto: number;
+    modello: string;
+    accessori: string;
+    difetto: string;
+    difettofisso: number;
+    stato: number;
+    difficolta: number;
+    descrizione: string;
+    note: string;
+    dataInizio: Date;
+    dataFine: Date;
+    riferimento: string;
+    nome: string;
+}
